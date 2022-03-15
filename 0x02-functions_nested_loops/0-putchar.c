0b094d9ce9419a270a601f880fbdf897cb76d8dc
dagmawitheodros <dagemtedi22@gmail.com>
2022-03-15 12:43:01 -0700

    function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - writing a program that prints Holberton followed by a new line.
+ *Return: The value 0 is returned.
+ */
+
+int main(void)
+
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
