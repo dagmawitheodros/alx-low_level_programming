ee7be509f9d018661a0e30fa145e27890f592641
dagmawitheodros <dagemtedi22@gmail.com>
2022-03-15 05:07:21 -0700

    function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+/**
+ *main - writing a program that prints Putchar followed by a new line.
+ *Return: The value 0 is returned.
+ */
+
+int main(void)
+
+{
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
